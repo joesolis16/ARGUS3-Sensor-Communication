509f7a77af333d87e21f4073e59263c546a12a7b
dracestone <anthony_duyan22@yahoo.com>
2023-03-22 17:09:11 -0700

    Reupload Receiver

--- lora_receiver/lora_receiver.ino
+++ lora_receiver/lora_receiver.ino
@@ -1,56 +1,28 @@
-/*This Code is for the RECEIVER*/
-
-// #define LED 5
+/*  
+  =========================================================================
+  Utilization: ARGUS III Lora Transceiver Test (Receiver)
+  Programmer 1: Anthony B. Duyan Jr. (RedID: 824809096)
+  Programmer 2: Joseph Solis (RedID: 824761763)
+  Date: 3/22/23
+  =========================================================================
+*/
 String received;
-
+const String msgRecv = "+RCV=";
+//  Setup code to run once.
 void setup() 
 {
-  // put your setup code here, to run once:
-  Serial.begin(115200);   //default baudrate of module is 115200
-  delay(2000);             //wait for Lora device to be ready
-
-  Serial.print("AT\r\n");
-  delay(1000);
   
-  Serial.print("AT+PARAMETER=10,7,1,7\r\n");      //For Less than 3Kms
-  //Serial.print("AT+PARAMETER=10,7,1,7\r\n");    //For More than 3Kms
-  delay(100);   //wait for module to respond
-  
-  Serial.print("AT+BAND=915000000\r\n");          //Bandwidth set to 868.5MHz
-  delay(100);   //wait for module to respond
-  
-  Serial.print("AT+ADDRESS=117\r\n");             //needs to be unique
-  delay(100);   //wait for module to respond
-  
-  Serial.print("AT+NETWORKID=8\r\n");             //needs to be same for receiver and transmitter
-  delay(100);   //wait for module to respond
-
-  // Serial.print("AT+PARAMETER?\r\n");              //For Less than 3Kms
-  // //Serial.print("AT+PARAMETER=10,7,1,7\r\n");    //For More than 3Kms
-  // delay(100);   //wait for module to respond
-  
-  // Serial.print("AT+BAND=915000000\r\n");          //Bandwidth set to 868.5MHz
-  // delay(100);   //wait for module to respond
-  
-  // Serial.print("AT+NETWORKID?\r\n");              //needs to be same for receiver and transmitter
-  // delay(100);   //wait for module to respond
-  
-  // Serial.print("AT+ADDRESS?\r\n");                //needs to be unique
-  // delay(100);   //wait for module to respond
-
-  // pinMode(LED,OUTPUT);
-  // digitalWrite(LED,LOW);
+  Serial.begin(115200);     // Default baudrate of module is 115200
 }
 
 void loop() 
 {
+
   
-  // put your main code here, to run repeatedly:
   if(Serial.available())
   {
     received = Serial.readString();
-    
-    Serial.println(received);
-    
+    if (received.startsWith(msgRecv))
+      Serial.println(received);
   }
 }
--- lora_transmitter_new/lora_transmitter_new.ino
+++ lora_transmitter_new/lora_transmitter_new.ino
@@ -112,10 +112,7 @@ void loop()
     Serial.print(sndMessage + msgLength + "," + values + "\r\n");
 
   }
-  for(int i = 0; i<= 2; i++)
-  {
-    LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,SPI_OFF, USART0_OFF, TWI_OFF);
-  }
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,SPI_OFF, USART0_OFF, TWI_OFF);
 }
 
 double readSensor(double* temperature)
